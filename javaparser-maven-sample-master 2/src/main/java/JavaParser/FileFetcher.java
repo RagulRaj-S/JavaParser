e2ad0c958ea3d806cd94f1099ee177e5359995bf
Ragul Raj <70879071+RagulRaj-S@users.noreply.github.com>
2021-11-16 13:12:01 +0530

    Update FileFetcher.java

--- javaparser-maven-sample-master 2/src/main/java/JavaParser/FileFetcher.java
+++ javaparser-maven-sample-master 2/src/main/java/JavaParser/FileFetcher.java
@@ -1,4 +1,22 @@
 package JavaParser;
 
 public class FileFetcher {
+    File sourceFile;
+    String filePath;
+    FileFetcherFactory factoryObject = new FileFetcherFactory();
+    Loader loaderObject;
+
+    // Checks the file path, select the respective class and loads the file from file path.
+
+    public File fetchFile() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the FilePath: ");
+        filePath = in.nextLine();
+
+        loaderObject = factoryObject.checkFile(filePath);
+        sourceFile = loaderObject.loadFile(filePath);
+
+        return sourceFile;
+    }
 }
